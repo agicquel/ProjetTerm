c1c6b4b3b23dc210be89c1cfd8f69e5ae85dd70e
Antoine Gicquel <antoine.gicquel@mailoo.org>
2015-12-07 16:56:14 +0100

    fin du cours

--- GUI/src/main.c
+++ GUI/src/main.c
@@ -6,14 +6,29 @@
 
 #include <gtk/gtk.h>
 
+#define height 600
+#define width 800
+
 int main(int argc, char *argv[]) {
 
-    gtk_init(&argc, &argv);
+	GtkWidget *fenetre;
+		
+	gtk_init(&argc, &argv);
 
-    g_printf("GTK+ version: %d.%d.%d\n", gtk_major_version, 
-        gtk_minor_version, gtk_micro_version);
-    g_printf("Glib version: %d.%d.%d\n", glib_major_version,
-        glib_minor_version, glib_micro_version);    
+	fenetre = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	gtk_window_set_title(GTK_WINDOW(fenetre), "Purificateur d'eau");
+	gtk_window_set_default_size(GTK_WINDOW(fenetre), width, height);
+	gtk_window_set_position(GTK_WINDOW(fenetre), GTK_WIN_POS_CENTER);
+
+	gtk_widget_show(fenetre);
+
+	g_signal_connect(fenetre, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	gtk_main();
+
+	g_printf("GTK+ version: %d.%d.%d\n", gtk_major_version, gtk_minor_version, gtk_micro_version);
+	g_printf("Glib version: %d.%d.%d\n", glib_major_version, glib_minor_version, glib_micro_version);    
         
     return 0;
 }
