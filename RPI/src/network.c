0fd730d478a4a5fb568d9ec36ab3db648ca7a410
Antoine Gicquel <antoine.gicquel@mailoo.org>
2016-01-25 16:59:44 +0100

    network

--- RPI/src/network.c
+++ RPI/src/network.c
@@ -11,29 +11,54 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <strings.h>
 
 #include "analyse.h"
-
-#define MAXPENDING 5    /* Max connection requests */
-#define BUFFSIZE 32
 
 void *network()
 {
   printf("Network Thread\n");
-  /*
-  int val = 1;
-  while (val)
-  {
-    scanf("%d", &val);
-  }
-  pthread_exit (NULL);
-  */
 
   int port = getConfiguration("port"); /* port number */
-
   printf("\nport = %d\n", port);
+  int sockfd, newsockfd;
+  socklen_t clilen;
+  char buffer[256];
+  struct sockaddr_in serv_addr, cli_addr;
+  int n;
 
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+  {
+    fprintf(stderr, "ERROR opening socket\n");
+  }
+  bzero((char *) &serv_addr, sizeof(serv_addr));
 
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+  {
+    fprintf(stderr, "ERROR on binding\n");
+  }
+
+  listen(sockfd,1);
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+  if (newsockfd < 0)
+  {
+    fprintf(stderr, "ERROR on accept\n");
+  }
+  bzero(buffer,256);
+
+  while ((strcmp(buffer, "quitter") != 0) || n != 0)
+  {
+    n = read(newsockfd,buffer,255 || n != 0);
+    printf("Here is the message: %s\n",buffer);
+  }
+
+  close(newsockfd);
+  close(sockfd);
   pthread_exit (NULL);
 }
 //sendAnalyse
